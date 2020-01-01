49712f6c8e29639fe90584cb1493c1afc03a25cf
AdelinaStana <stana.adelina.diana@gmail.com>
2020-01-02 00:11:00 +0200

    include package name for enities names

--- NameTagParser.py
+++ NameTagParser.py
@@ -7,6 +7,7 @@ from AttributeModel import AttributeModel
 class NameTagParser(Parser):
     def __init__(self, root_dir, unique_id):
         Parser.__init__(self, root_dir, unique_id)
+        self.replace = "org"
 
     def get_all_structures(self, root):
         item_list = []
@@ -33,6 +34,17 @@ class NameTagParser(Parser):
     def get_class_model(self, file, item_list):
         class_list = []
 
+        file_path = file.replace(self.working_dir, 'a/')
+        file_path = file_path.replace(".xml", "")
+        file_path = file_path.replace("\\", "/")
+
+        rep = file_path.find(self.replace)
+        rep = file_path[0:rep]
+        base = file_path.replace(rep, "")
+        extension = os.path.splitext(base)[1]
+        base = base.replace(extension, "")
+        base = base.replace("/", ".")
+
         for item in item_list:
             class_model = ClassModel()
             class_model.set_name(self.get_name(item))
@@ -40,9 +52,10 @@ class NameTagParser(Parser):
             if class_model.name != "None":
                 self.unique_id += 1
 
-                file_path = file.replace(self.working_dir, 'a/')
-                file_path = file_path.replace(".xml", "")
-                file_path = file_path.replace("\\", "/")
+                file_name = base
+                if not base.endswith(class_model.name):
+                    file_name = base + "$" + class_model.name
+                print(file_name)
 
                 class_model.set_file(file_path)
                 class_model.set_unique_id(self.unique_id)
@@ -53,6 +66,7 @@ class NameTagParser(Parser):
                     class_model.add_attribute(attribute)
 
                 class_list.append(class_model)
+                # print(class_model.name)
 
         return class_list
 
