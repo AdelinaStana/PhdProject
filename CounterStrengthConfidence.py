276d92464d38f96956dc6d5ac1fbada266d7a80c
AdelinaStana <stana.adelina.diana@gmail.com>
2022-02-10 22:18:34 +0200

    change confidence calculation

--- CounterStrengthConfidence.py
+++ CounterStrengthConfidence.py
@@ -49,7 +49,7 @@ class CounterStrengthConfidence(CounterStrength):
 
     def count_strength(self, pos, threshold):
         entity_class_id_dict = {}
-        max_occ_set = set()
+        max_occ_list = []
 
         for classItem in self.structure_manager.get_class_list():
             entity_class_id_dict[classItem.unique_id] = classItem
@@ -57,39 +57,42 @@ class CounterStrengthConfidence(CounterStrength):
             max_occ = 0
             if values:
                 max_occ = max(values)
-            max_occ_set.add(max_occ)
+            max_occ_list.append(max_occ)
 
-        mean = statistics.mean(list(max_occ_set))
+        mean = statistics.mean(max_occ_list)
         print(mean)
 
-        g = Graph(self.working_dir + "\\" + self.name + "_git_strength_"+str(threshold), self.structure_manager)
+        count = [i for i in max_occ_list if i > mean]
+        print(len(count))
+        csv_name = self.working_dir + "\\" + self.name + "_git_strength_"+str(threshold) + ".csv"
+        file_writer = open(csv_name, 'wt')
+        file_writer.write("a,b,c\n")
+
+        entities_count = 0
         try:
             for classItem in self.structure_manager.get_class_list():
                 entity1 = classItem
-                entity1_id = classItem.unique_id
 
                 related_list = classItem.get_filtered_commit_size_occurrences(1)
                 for entity2_id in related_list:
                     entity2 = entity_class_id_dict[entity2_id]
 
-                    nr_of_commits_together1 = entity1.get_nr_of_occ_with(entity2_id)  # commits involving A and B
-                    nr_of_total_commits1 = entity1.commits_count  # total nr of commits involving A
+                    freqAUB = entity1.get_nr_of_occ_with(entity2_id)  # commits involving A and B
+                    freqA = entity1.commits_count  # total nr of commits involving A
 
-                    nr_of_commits_together2 = entity2.get_nr_of_occ_with(entity1_id)  # commits involving A and B
-                    nr_of_total_commits2 = entity2.commits_count  # total nr of commits involving B
+                    strength = freqAUB/mean
 
-                    confidence1 = nr_of_total_commits1/mean
-                    confidence2 = nr_of_total_commits2/mean
+                    confidence_percent = ((100 * freqAUB) / freqA) * strength
 
-                    update_percentage1 = ((100 * nr_of_commits_together1) / nr_of_total_commits1) * confidence1
-                    update_percentage2 = ((100 * nr_of_commits_together2) / nr_of_total_commits2) * confidence2
-
-                    if update_percentage1 >= threshold and update_percentage2 >= threshold:
-                        g.add_edge(classItem.unique_id, entity2_id)
+                    if confidence_percent >= threshold:
+                        file_writer.write(entity1.full_name + "," + entity2.full_name + "," +
+                                          str(round(confidence_percent, 0)) + "\n")
+                        entities_count += 1
 
         except BaseException as e:
             print("Exception: "+str(e))
 
-        self.results_count[pos] = g.number_of_edges()
-        g.export_names_to_csv()
+        file_writer.close()
+
+        self.results_count[pos] = entities_count
         print("Count git links with strength "+str(threshold)+"% ...")
